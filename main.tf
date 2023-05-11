ed7eede3dc7db5f5a8fd8bcd959955c9d35c5ec8
Ryan Christopher <ryanchristopher@Ryans-Air.attlocal.net>
2023-05-11 13:54:33 -0500

    changed main

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
 terraform{
   # Configure the AWS Provider
-  provider "aws" {
-    region = "us-east-1"
+  equired_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
   }
+  required_version = ">= 1.1.0"
   cloud {
     organization = "rdcresume"
 
